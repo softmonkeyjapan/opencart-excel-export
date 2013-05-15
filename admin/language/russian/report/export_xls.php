8fa5d64554c31b11f6b93c9961837fe407c08131
SoftMonkeyJapan <contact@soft-monkey.com>
2013-05-15 23:13:29 +0900

    Add Russian language (Translated by Ilsur Bilalov)

---
+++ admin/language/russian/report/export_xls.php
@@ -0,0 +1,52 @@
+<?php
+	$_['heading_title'] 				= 'Экспорт заказаов';
+
+	// Module text
+	$_['text_home'] 					= 'Главная';
+	$_['text_customer'] 				= 'Имя клиента';
+	$_['text_date']						= 'Дата покупки';
+	$_['text_order']					= 'ID заказа';
+	$_['text_amount']					= 'Общая сумма';
+	$_['text_product'] 					= 'Количество товара';
+	$_['text_status']					= 'Статус';
+	$_['text_action']     				= 'Действия';
+	$_['text_view'] 					= 'Просмотреть';
+	$_['text_export']					= 'Экспорт';
+	$_['text_invoice']					= 'Счет';
+	$_['text_export_all'] 				= 'Экспортировать все';
+	$_['text_all_status']  				= 'Все статусы';
+
+	// Entry filter text
+	$_['entry_date_start'] 				= 'Дата начала:';
+	$_['entry_date_end']				= 'Дата окончания:';
+	$_['entry_status']      			= 'Статус заказа:';
+	$_['button_filter']					= 'Фильтр';
+
+	// Header Excel worksheet text
+	$_['header_tracking'] 				= 'Номер отслеживания';
+	$_['header_order_id'] 				= 'ID заказа';
+	$_['header_store_name'] 			= 'Магазин';
+	$_['header_customer'] 				= 'Имя клиента';
+	$_['header_email'] 					= 'E-mail';
+	$_['header_telephone'] 				= 'Телефон';
+	$_['header_total'] 					= 'Итого (цена):';
+	$_['header_date'] 					= 'Дата покупки';
+	$_['header_product_name']			= 'Наименование';
+	$_['header_product_quantity']		= 'Количество';
+	$_['header_product_model']			= 'Модель';
+	$_['header_product_price']			= 'Итого';
+	$_['header_product_unit_price']		= 'Цена за еденицу';
+	$_['header_product_color']			= 'Цвет';
+	$_['header_product_size']			= 'Размер';
+	$_['header_shipping_firstname'] 	= 'Имя,Отчество';
+	$_['header_shipping_lastname'] 		= 'Фамилия';
+	$_['header_shipping_address'] 		= 'Адрес';
+	$_['header_shipping_city'] 			= 'Город';
+	$_['header_shipping_postcode'] 		= 'Индекс';
+	$_['header_shipping_zone'] 			= 'Регион / Область';
+	$_['header_shipping_country'] 		= 'Страна';
+	$_['header_shipping_method'] 		= 'Способ доставки';
+	$_['header_payment_method']			= 'Способ оплаты';
+	$_['header_to']						= 'Для';
+	$_['header_ship_to']				= 'Доставка (если другой адрес)';
+?>
